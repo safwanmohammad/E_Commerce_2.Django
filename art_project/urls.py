a141fc176eccac4d2cef1c9bff99854f1409dfb6
MOHAMMED SAFWAN <110681039+safwanmohammad@users.noreply.github.com>
2023-06-20 00:58:52 +0530

    added variations

--- art_project/urls.py
+++ art_project/urls.py
@@ -23,6 +23,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('cart/',include('carts.urls')),
     path('store/',include('store.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
--- store/models.py
+++ store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     stock          = models.IntegerField()
     is_available   = models.BooleanField(default=True)
     category       = models.ForeignKey(Category,on_delete=models.CASCADE)
-    craeted_date   = models.DateTimeField(auto_now_add=True)
+    created_date   = models.DateTimeField(auto_now_add=True)
     modified_date  = models.DateTimeField(auto_now=True)
 
     def get_url(self):
@@ -21,3 +21,31 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+class VariationMangaer(models.Manager):
+    def format(self):
+        return super(VariationMangaer,self).filter(variation_category='format',is_active=True)
+
+    def size(self):
+        return super(VariationMangaer,self).filter(variation_category='size',is_active=True)
+
+
+variation_category_choice = (
+    ('format','format'),
+    ('size','size'),
+    
+)
+
+
+class Variation(models.Model):
+    product            = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100,choices=variation_category_choice)
+    variation_value    = models.CharField(max_length=100)
+    is_active          = models.BooleanField(default=True)
+    created_date       = models.DateTimeField(auto_now=True)
+
+    objects = VariationMangaer()
+
+    def __str__(self):
+        return self.variation_value
